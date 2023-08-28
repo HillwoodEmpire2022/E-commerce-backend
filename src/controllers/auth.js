44fd17d26db7d2c5aed34e5c384f5df5a3b0ebf9
--replace-all <h.buterak@gmail.com>
2023-08-28 12:48:45 +0300

    add login route

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,6 +1,6 @@
 import User from "../models/User.js";
 import bcrypt from "bcrypt"
-import { signupValidationSchema } from "../validations/signupValidation.js";
+import { signupValidationSchema, loginValidationSchema } from "../validations/authValidations.js";
 import jwt from "jsonwebtoken"
 import dotenv from "dotenv"
 
@@ -50,7 +50,7 @@ export const userRegister = async (req, res) => {
         const savedUser = await User.create(newUser)
         const userToken = jwt.sign({
             id: savedUser._id
-        }, process.env.SECRET_KEY)
+        }, { expiresIn: '1h' }, process.env.SECRET_KEY)
 
         res.status(201).json({
             userToken,
@@ -60,4 +60,56 @@ export const userRegister = async (req, res) => {
     } catch (error) {
         res.status(500).json({ error: error.message })
     }
+}
+
+export const userLogin = async (req, res) => { 
+    try {
+        let { 
+            email,
+            password,
+        } = req.body
+
+        const { error } = loginValidationSchema.validate(
+            req.body,
+            { errors: { label: 'key', wrap: { label: false } } })
+        if (error) {
+            res.status(422).send({ message: error.message })
+        }
+        
+        let userInfo = {}
+        const account = await User.findOne({email:email})
+        if (!account) {
+            res.status(401).send({ message: "The email provided does not exist."})
+        } else {
+            
+            const passwordCheck = await bcrypt.compare(password, account.hashedPassword)
+            
+           
+            if (!passwordCheck) {
+                res.status(401).send({ message: "Wrong password."})
+            } else {
+                userInfo = {
+                    _id: account._id,
+                    firstname: account.firstname,
+                    lastname: account.lastname,
+                    username: account.username,
+                    role: account.role,
+                    profileImageUrl: account.profileImageUrl,
+
+                } 
+
+                const userToken = jwt.sign({
+                    _id: account._id
+                }, { expiresIn: '1h' }, process.env.SECRET_KEY)
+
+                res.status(200).json({
+                    token: userToken,
+                    user: userInfo,
+                })
+            }
+        }
+
+    } catch (error) {
+        res.status(500).json({ error: error.message })
+    }  
 }
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,9 +1,31 @@
 import express from "express";
-import { userRegister } from "../controllers/user.js"
+import { userRegister, userLogin } from "../controllers/auth.js"
+import passport from "passport"
 
 
 const router = express.Router()
 
-router.post("/user/account/register", userRegister)
+router.post("/user/register", userRegister)
+router.post("/user/login", userLogin)
+router.get("/google", passport.authenticate("google", { scope: ["profile"] }))
+router.get("/google/callback", passport.authenticate("google",
+    {
+        scope: ["email","profile"] ,
+        failureRedirect: '/auth/google/failure'
+    }), (req, res) => {
+  
+        console.log("accout");
+        res.status(201).json({
+            message: "Your account has been created"
+        })
+})
+
+
+router.get("/auth/google/failure", (req, res) => { 
+    console.log(failed);
+    res.status(401).json({
+        message: "Unable to sign in using Google, please try again later"
+    })
+})
 
 export default router
