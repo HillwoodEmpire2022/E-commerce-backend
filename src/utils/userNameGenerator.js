21cebbe3ca046c2796469e0f0d041f6cf5951e3d
--replace-all <h.buterak@gmail.com>
2023-08-29 15:07:40 +0300

    --amend

---
+++ src/utils/userNameGenerator.js
@@ -0,0 +1,16 @@
+import User from "../models/User.js"
+
+export const generateUserName = async () => { 
+    const recentRegisteredUser = await User.find().sort({ _id: -1 }).limit(1)
+    let newUserName = ""
+
+    if (recentRegisteredUser.length !== 0) {
+        const collectionCount = recentRegisteredUser[0].username.slice(4)
+        newUserName = `user${parseInt(collectionCount) + 1}`
+    } else {
+        newUserName = `user${1}`      
+    }
+
+    return newUserName
+}
+
