fb3cf14eb0ab55fe9ace37a5b31ce63f0ebfc282
--replace-all <h.buterak@gmail.com>
2023-08-31 16:17:01 +0300

    fix retrieving user information on successful google authentication

--- src/utils/passportConfig.js
+++ src/utils/passportConfig.js
@@ -3,7 +3,7 @@ import { Strategy } from "passport-google-oauth20";
 import User from "../models/user.js";
 import { generateUserName } from "./userNameGenerator.js";
 
-export const passportConfig = () => {
+export const passportConfig = async () => {
   passport.use(
     new Strategy(
       {
@@ -14,6 +14,7 @@ export const passportConfig = () => {
       (accessToken, refreshToken, profile, done) => {
         User.findOne({ googleId: profile.id })
           .then((user) => {
+            console.log(user, "log 1")
             if (!user) {
               generateUserName()
                 .then((userName) => {
@@ -25,7 +26,8 @@ export const passportConfig = () => {
                     email: profile._json.email,
                   });
                   User.create(user).then((err, user) => {
-                    return done(null, user);
+                    console.log(user, "log 2");
+                    return done(user);
                   });
                 })
                 .catch((err) => {
