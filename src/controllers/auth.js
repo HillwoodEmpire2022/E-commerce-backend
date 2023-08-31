2b82647171a20a332a24a3943d9499a7575304d9
--replace-all <h.buterak@gmail.com>
2023-08-31 14:38:24 +0300

    fix PORT caused failures

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,122 +1,116 @@
-import User from "../models/User.js";
-import bcrypt from "bcrypt"
-import { signupValidationSchema, loginValidationSchema } from "../validations/authValidations.js";
-import dotenv from "dotenv"
-import { generateUserName } from "../utils/userNameGenerator.js"
+import User from "../models/user.js";
+import bcrypt from "bcrypt";
+import {
+  signupValidationSchema,
+  loginValidationSchema,
+} from "../validations/authValidations.js";
+import dotenv from "dotenv";
+import { generateUserName } from "../utils/userNameGenerator.js";
 import generateJWToken from "../utils/jwToken.js";
 
-dotenv.config()
+dotenv.config();
 
-export const userRegister = async (req, res) => { 
-    try {
-        let { 
-            firstname,
-            lastname,
-            email,
-            password,
-        } = req.body
+export const userRegister = async (req, res) => {
+  try {
+    let { firstname, lastname, email, password } = req.body;
 
-        const { error } = signupValidationSchema.validate(
-            req.body,
-            { errors: { label: 'key', wrap: { label: false } } })
-        if (error) {
-            res.status(422).send({ message: error.message })
-            return
-        }
-        const existingUser = await User.findOne({ email: email })
-        
-        if (existingUser && existingUser.googleId === null) { 
-            res.status(409).json({ message: `A user with email: ${email} already exists.` })
-            return
-        }
-
-        const salt = await bcrypt.genSalt()
-        const passwordHarsh = await bcrypt.hash(password, salt)
-
-        
-
-        firstname = firstname.charAt(0).toUpperCase() + firstname.slice(1).toLowerCase()
-        lastname = lastname.charAt(0).toUpperCase()+lastname.slice(1).toLowerCase()
-
-        const newUserName = await generateUserName();
-
-        const newUser = {
-            firstname,
-            lastname,
-            username: newUserName,
-            email,
-            hashedPassword: passwordHarsh, 
-        }
-
-        const savedUser = await User.create(newUser)
-        const userToken = generateJWToken(savedUser._id)
-
-        res.status(201).json({
-            userToken,
-            user: savedUser
-        })
-
-    } catch (error) {
-        res.status(500).json({ error: error.message })
+    const { error } = signupValidationSchema.validate(req.body, {
+      errors: { label: "key", wrap: { label: false } },
+    });
+    if (error) {
+      res.status(422).send({ message: error.message });
+      return;
     }
-}
+    const existingUser = await User.findOne({ email: email });
 
-export const userLogin = async (req, res) => { 
-    try {
-        let { 
-            email,
-            password,
-        } = req.body
+    if (existingUser && existingUser.googleId === null) {
+      res
+        .status(409)
+        .json({ message: `A user with email: ${email} already exists.` });
+      return;
+    }
 
-        const { error } = loginValidationSchema.validate(
-            req.body,
-            { errors: { label: 'key', wrap: { label: false } } })
-        if (error) {
-            res.status(422).send({ message: error.message })
-        }
-        
-        let userInfo = {}
-        const account = await User.findOne({email:email})
-        if (!account) {
-            res.status(401).send({ message: "The email provided does not exist."})
-        } else {
-            
-            const passwordCheck = await bcrypt.compare(password, account.hashedPassword)
-            
-           
-            if (!passwordCheck) {
-                res.status(401).send({ message: "Wrong password."})
-            } else {
-                userInfo = {
-                    _id: account._id,
-                    firstname: account.firstname,
-                    lastname: account.lastname,
-                    username: account.username,
-                    role: account.role,
-                    profileImageUrl: account.profileImageUrl,
+    const salt = await bcrypt.genSalt();
+    const passwordHarsh = await bcrypt.hash(password, salt);
 
-                } 
+    firstname =
+      firstname.charAt(0).toUpperCase() + firstname.slice(1).toLowerCase();
+    lastname =
+      lastname.charAt(0).toUpperCase() + lastname.slice(1).toLowerCase();
 
-                const userToken = generateJWToken(account._id)
+    const newUserName = await generateUserName();
 
-                res.status(200).json({
-                    token: userToken,
-                    user: userInfo,
-                })
-            }
-        }
+    const newUser = {
+      firstname,
+      lastname,
+      username: newUserName,
+      email,
+      hashedPassword: passwordHarsh,
+    };
 
-    } catch (error) {
-        res.status(500).json({ error: error.message })
-    }  
-}
+    const savedUser = await User.create(newUser);
+    const userToken = generateJWToken(savedUser._id);
 
-export const googleAuthenticationSuccess = (req, res) => { 
-    const userToken = generateJWToken(req.user.id)
-    
-    res.status(200).json({
-        token: userToken,
-        user: req.user,
-    })
-}
+    res.status(201).json({
+      userToken,
+      user: savedUser,
+    });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
 
+export const userLogin = async (req, res) => {
+  try {
+    let { email, password } = req.body;
+
+    const { error } = loginValidationSchema.validate(req.body, {
+      errors: { label: "key", wrap: { label: false } },
+    });
+    if (error) {
+      res.status(422).send({ message: error.message });
+    }
+
+    let userInfo = {};
+    const account = await User.findOne({ email: email });
+    if (!account) {
+      res.status(401).send({ message: "The email provided does not exist." });
+    } else {
+      const passwordCheck = await bcrypt.compare(
+        password,
+        account.hashedPassword
+      );
+
+      if (!passwordCheck) {
+        res.status(401).send({ message: "Wrong password." });
+      } else {
+        userInfo = {
+          _id: account._id,
+          firstname: account.firstname,
+          lastname: account.lastname,
+          username: account.username,
+          role: account.role,
+          profileImageUrl: account.profileImageUrl,
+        };
+
+        const userToken = generateJWToken(account._id);
+
+        res.status(200).json({
+          token: userToken,
+          user: userInfo,
+        });
+      }
+    }
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+export const googleAuthenticationSuccess = (req, res) => {
+  const userToken = generateJWToken(req.user.id);
+
+  res.status(200).json({
+    token: userToken,
+    user: req.user,
+  });
+};
--- src/utils/passportConfig.js
+++ src/utils/passportConfig.js
@@ -1,6 +1,6 @@
 import passport from "passport";
 import { Strategy } from "passport-google-oauth20"
-import User from "../models/User.js";
+import User from "../models/user.js";
 import { generateUserName } from "./userNameGenerator.js"
 
 
