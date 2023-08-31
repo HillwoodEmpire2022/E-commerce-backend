e9c5f56fd41d9d78de4b12e14e6403cb1de85a55
--replace-all <h.buterak@gmail.com>
2023-08-31 16:17:01 +0300

    fix retrieving user information on successful google authentication

--- src/middlewares/loggedInCheck.js
+++ src/middlewares/loggedInCheck.js
@@ -1,5 +1,6 @@
 export const isLoggedIn = (req, res, next) => {
-    req.user ? next()
+    console.log("log", req.returnPayload);
+    req.returnPayload ? next()
         : res.status(401).send({
             message: "You are unauthorized. Please login again"
         })
