5ceb6790cd36ab75643998d485c07cfaffa164c0
--replace-all <h.buterak@gmail.com>
2023-08-31 16:17:01 +0300

    fix retrieving user information on successful google authentication

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -69,6 +69,7 @@ export const userLogin = async (req, res) => {
     });
     if (error) {
       res.status(422).send({ message: error.message });
+      return
     }
 
     let userInfo = {};
@@ -107,10 +108,30 @@ export const userLogin = async (req, res) => {
 };
 
 export const googleAuthenticationSuccess = (req, res) => {
-  const userToken = generateJWToken(req.user.id);
+  try { 
+    const user = req.user
+    const displayedUserInfo = {
+      _id: user._id,
+      firstname: user.firstname,
+      lastname: user.lastname,
+      username: user.username,
+      role: user.role,
+      profileImageUrl: user.profileImageUrl,
+    }
 
-  res.status(200).json({
-    token: userToken,
-    user: req.user,
-  });
+    const userToken = generateJWToken(user.id);
+    console.log(" log", displayedUserInfo);
+    console.log(JSON.stringify({
+      token: userToken,
+      user: displayedUserInfo
+    }));
+    res.status(200).json({
+      token: userToken,
+      user: displayedUserInfo
+    })   
+
+  } catch (err) {
+    res.status(500).json({ err: err.message})
+  }
+
 };
