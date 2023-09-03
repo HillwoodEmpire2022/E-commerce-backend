d379337c3b490cc774daa5f8af07ea95eff8d1ac
--replace-all <h.buterak@gmail.com>
2023-09-03 10:51:52 +0300

    remove storing google id in the coockies sessions

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -5,8 +5,6 @@ import {
   googleAuthenticationSuccess,
 } from "../controllers/auth.js";
 import passport from "passport";
-import generateJWToken from "../utils/jwToken.js";
-import isLoggedIn from "../middlewares/loggedInCheck.js";
 
 const router = express.Router();
 
@@ -23,13 +21,10 @@ router.get(
     failureRedirect: "/auth/google/failure",
   })
 );
-// console.log(req.user);
 
 router.get("/auth/google/success", googleAuthenticationSuccess);
 
 router.get("/auth/google/failure", (req, res) => {
-  console.log(req.err)
-  console.log("failed");
   res.status(401).json({
     message: "Unable to sign in using Google, please try again later",
   });
