0efec9e36c976f960f5655ce55fb0796bd256bbe
--replace-all <h.buterak@gmail.com>
2023-09-11 19:18:24 +0300

    implement uploading and getting products

--- src/controllers/product.js
+++ src/controllers/product.js
@@ -8,6 +8,7 @@ export const uploadNewProduct = async (req, res) => {
   try {
     const { error } = uploadProductValidation.validate(req.body, {
       errors: { label: "key", wrap: { label: false } },
+      allowUnknown: true
     });
     if (error) {
       res.status(422).send({ message: error.message });
@@ -28,25 +29,26 @@ export const uploadNewProduct = async (req, res) => {
       return res.status(409).send({ message: "You have already uploaded this same product."})
     }
 
-    let productThumbnail = req.file;
-    
+    let productThumbnail = req.files.productThumbnail[0];
     if (!productThumbnail) { 
       return res.status(400).send({message: "There is no thumbnail image attached."})
     }
-    const uploadedThumbnail = uploadToCloudinary(productThumbnail.path, seller.companyName, req.body.name, "productThumbnail")
-  
+    const uploadedThumbnail = await uploadToCloudinary(productThumbnail.path, seller.companyName, req.body.name, "productThumbnail")
+
     let otherImages = req.files.otherImages;
-    console.log(otherImages);
+
     if (!otherImages || otherImages.length === 0) { 
       return res.status(400).send({message: "There is no any image for otherImages"})
     }
-    const uploadedOtherImages = otherImages.map(image => { 
-      let uploadedImage = uploadToCloudinary(image.path, seller.companyName, req.body.name, "otherImages")
-      return { public_id: uploadedImage.public_id, url: uploadedImage.url }
-    })
+
+    let uploadedOtherImages = []
+    for (let i = 0; i < otherImages.length; i++) {
+      let uploadedImage = await uploadToCloudinary(otherImages[i].path, seller.companyName, req.body.name, "otherImages")
+      uploadedOtherImages[i] = { public_id: uploadedImage.public_id, url: uploadedImage.url }
+    }
   
     let colorImages = req.files.colorImages;
-    console.log(colorImages);
+
     if (!colorImages || colorImages.length === 0) { 
       return res.status(400).send({message: "There is no any image for colorImages"})
     }
@@ -54,7 +56,7 @@ export const uploadNewProduct = async (req, res) => {
     let uploadedColorImages = []
     for (let i = 0; i < colorImages.length; i++) {
       let uploadedImage = await uploadToCloudinary(colorImages[i].path, seller.companyName, req.body.name, "colorImages")
-      uploadedColorImages[i] = { public_id: uploadedImage.public_id, url: uploadedImage.url, colorName: colorImages[i].colorName}
+      uploadedColorImages[i] = { public_id: uploadedImage.public_id, url: uploadedImage.url, colorName: req.body.colorNames[i] }
     }
   
     let discountedPrice = req.body.price - (req.body.discountPercentage/100)*req.body.price
@@ -74,4 +76,60 @@ export const uploadNewProduct = async (req, res) => {
     res.status(500).send({ message: error.message })
   }
 }
- 
+
+export const getAllProducts = async (req, res) => { 
+  try {
+    const allProducts = await Product.find()
+      .populate("category", "name")
+      .populate("subcategory", "name").exec()
+    
+    if (allProducts.length === 0) {
+      return res.status(404).send({ message: "There are no products available." })
+    }
+    res.status(200).json(allProducts)
+
+  } catch (error) {
+    res.status(500).send({ error: error.message })
+  }
+}
+
+export const getSingleProduct = async (req, res) => {
+  try {
+    const product = await Product.findOne({ _id: req.params.productId })
+      .populate("seller", "companyName")
+      .exec()
+    
+    if (!product) { 
+      return res.status(404).send({ message: "Product not found."})
+    }
+    res.status(200).json(product)
+  } catch (error) {
+    res.status(500).send({ error: error.message })
+  }
+}
+
+export const getProductsByCategory = async (req, res) => { 
+  try {
+    const products = await Product.find({ category: req.params.categoryId })
+    
+    if (products.length === 0 ) { 
+      return res.status(404).send({ message: "No products belonging in this category."})
+    }
+    res.status(200).json(products)
+  } catch (error) {
+    res.status(500).send({ error: error.message })
+  }
+}
+
+export const getProductsBySubCategory = async (req, res) => { 
+  try {
+    const products = await Product.find({ subcategory: req.params.subcategoryId })
+    
+    if (products.length === 0 ) { 
+      return res.status(404).send({ message: "No products belonging in this sub category."})
+    }
+    res.status(200).json(products)
+  } catch (error) {
+    res.status(500).send({ error: error.message })
+  }
+}
--- src/routes/product.js
+++ src/routes/product.js
@@ -1,16 +1,22 @@
 import express from "express"
 
 import {  isAdmin } from "../middlewares/auth.js"
-import { uploadNewProduct } from "../controllers/product.js"
+import { getAllProducts, getProductsByCategory, getProductsBySubCategory, getSingleProduct, uploadNewProduct } from "../controllers/product.js"
 import { upload } from "../utils/multer.js"
 
 const Router = express.Router()
 
 Router.post("/product/upload", 
-    upload.single("productThumbnail"),
-    upload.array("otherImages", 6),
-    upload.array("colorImages", 6),
     isAdmin,
+       upload.fields(([
+        { name: 'productThumbnail', maxCount: 1 },
+        { name: 'otherImages', maxCount: 6 },
+        { name: 'colorImages', maxCount: 6 },
+      ])),
     uploadNewProduct)
+Router.get("/products", getAllProducts) 
+Router.get("/product/:productId", getSingleProduct) 
+Router.get("/products/category/:categoryId", getProductsByCategory)
+Router.get("/products/subcategory/:subcategoryId", getProductsBySubCategory)
 
 export default Router
