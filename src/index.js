6bd88db1681b88a548430fd2f6f162c24bcb68c1
--replace-all <h.buterak@gmail.com>
2023-09-14 19:15:22 +0300

    fix CORS errors

--- src/index.js
+++ src/index.js
@@ -21,7 +21,9 @@ const app = express()
 app.use(express.json({}))
 app.use(express.urlencoded({ extended: true }))
 app.use(helmet())
-app.use(cors())
+app.use(cors({
+    origin: "*"
+}))
 app.use(morgan("common"))
 
 passportConfig()
