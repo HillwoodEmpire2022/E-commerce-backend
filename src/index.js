81041dd2a0a7ced0e52be6cb5351939422ce5db1
--replace-all <h.buterak@gmail.com>
2023-10-21 11:47:15 +0300

    fix CORS policy blocks

--- src/index.js
+++ src/index.js
@@ -18,13 +18,16 @@ dotenv.config()
 
 const app = express()
 
-app.use(express.json({}))
-app.use(express.urlencoded({ extended: true }))
-app.use(helmet())
 app.use(cors({
     origin: "https://classy-salamander-0a7429.netlify.app", 
     credentials: true, 
+    methods: ["GET", "POST", "PUT", "DELETE"],
   }));
+
+app.use(express.json({}))
+app.use(express.urlencoded({ extended: true }))
+app.use(helmet())
+
 app.use(morgan("common"))
 
 passportConfig()
