f5cf397b2b8358c1d1d24edef30f1410462261db
--replace-all <h.buterak@gmail.com>
2023-10-21 10:28:36 +0300

    fix CORS missing origin url

--- src/index.js
+++ src/index.js
@@ -22,7 +22,7 @@ app.use(express.json({}))
 app.use(express.urlencoded({ extended: true }))
 app.use(helmet())
 app.use(cors({
-    origin: "*", 
+    origin: "https://classy-salamander-0a7429.netlify.app", 
     credentials: true, 
   }));
 app.use(morgan("common"))
