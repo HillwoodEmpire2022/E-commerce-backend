caea220859dcc1055b50a125c28ff322daad3323
--replace-all <h.buterak@gmail.com>
2023-11-08 09:41:20 +0200

    fix typo of .senofd instead of .send

--- src/controllers/productsCart.js
+++ src/controllers/productsCart.js
@@ -83,7 +83,7 @@ export const getCartItems = async (req, res) => {
         if (formattedCartItems.length > 0) {
             return res.status(200).json(formattedCartItems)
         } else { 
-            return res.status(400).senofd({message: "There isn't any item in the cart. Explore items to add to cart."})
+            return res.status(400).send({message: "There isn't any item in the cart. Explore items to add to cart."})
         }
     } catch (error) {
         return res.status(500).send({ message: error.message })
