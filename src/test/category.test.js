caae9232c4088126fe935366c88d1b5d0b5eff48
Eric Tuyizere Ndungutse <dav.ndungutse@gmail.com>
2023-12-21 16:07:07 +0200

    setup category testing

--- src/test/category.test.js
+++ src/test/category.test.js
@@ -1,5 +1,7 @@
 describe("Test Configuration", () => {
   test("If it returns null", () => {
+    console.log(process.env.NODE_ENV);
+    console.log(process.env.TEST_DB);
     const n = null;
     expect(n).toBeNull();
     expect(n).toBeDefined();
