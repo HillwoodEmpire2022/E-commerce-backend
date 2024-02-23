b60c8e6b391d8fcf90ab4babc3ca8743df5b9bec
ericndungutse <dav.ndungutse@gmil.com>
2024-02-23 17:09:38 +0200

    update user by admin

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,5 +1,6 @@
 import User from '../models/user.js';
 import APIFeatures from '../utils/APIFeatures.js';
+import { updateUserSchema } from '../validations/userUpdateValidation.js';
 
 export const getUsers = async (req, res) => {
   try {
@@ -25,5 +26,36 @@ export const getUsers = async (req, res) => {
       status: 'error',
       message: 'internal server error! Please try again.',
     });
+  }
+};
+
+export const updateUser = async (req, res) => {
+  const userId = req.params.id;
+  try {
+    const { error } = updateUserSchema.validate(req.body, {
+      errors: { label: 'key', wrap: { label: false } },
+      allowUnknown: true,
+    });
+
+    if (error) {
+      return res.status(400).json({ status: 'fail', message: error.message });
+    }
+
+    // Fetch the user by ID
+    console.log(userId);
+    const user = await User.findByIdAndUpdate(userId, req.body, {
+      new: true,
+    });
+
+    if (!user) {
+      return res
+        .status(404)
+        .json({ status: 'fail', message: 'User not found' });
+    }
+
+    res.status(200).json({ status: 'success', data: user });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Internal server error' });
   }
 };
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { getUsers } from '../controllers/user.js';
+import { getUsers, updateUser } from '../controllers/user.js';
 import { isLoggedIn } from '../middlewares/authentication.js';
 import { restrictTo } from '../middlewares/authorization.js';
 
 const router = express.Router();
 
 router.get('/', isLoggedIn, restrictTo('admin'), getUsers);
+router.patch('/:id', isLoggedIn, restrictTo('admin'), updateUser);
 
 export default router;
