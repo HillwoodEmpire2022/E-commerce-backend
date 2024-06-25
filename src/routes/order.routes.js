b091bab619be3c8c48c95a30cae51bba4f9773fb
ericndungutse <dav.ndungutse@gmail.com>
2024-06-25 10:58:06 +0200

    fix: Fix customer's inability to view his orders

--- src/routes/order.routes.js
+++ src/routes/order.routes.js
@@ -1,17 +1,12 @@
 import express from 'express';
 import { isLoggedIn } from '../middlewares/authentication.js';
-import {
-  getOrder,
-  getOrders,
-  updateOrder,
-} from '../controllers/order.js';
+import { getOrder, getOrders, updateOrder } from '../controllers/order.js';
 import { restrictTo } from '../middlewares/authorization.js';
 
 const router = express.Router({ mergeParams: true });
 
-router.use(isLoggedIn, restrictTo('admin'));
-router.get('/', getOrders);
-router.patch('/:id', updateOrder).get(getOrder);
-router.get('/:id', getOrder);
+router.get('/', isLoggedIn, restrictTo('admin', 'customer'), getOrders);
+router.patch('/:id', isLoggedIn, restrictTo('admin'), updateOrder).get(getOrder);
+router.get('/:id', isLoggedIn, restrictTo('admin'), getOrder);
 
 export default router;
