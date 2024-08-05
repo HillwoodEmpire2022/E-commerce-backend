709a7662b26746b4b04e830cc48a4bb00d5cfed4
ericndungutse <dav.ndungutse@gmail.com>
2024-08-05 15:53:15 +0200

    feat: Style email containg link to reset password

--- src/utils/email.js
+++ src/utils/email.js
@@ -1,18 +1,25 @@
 import { Resend } from 'resend';
 import dotenv from 'dotenv';
 dotenv.config();
-import { activationEmailTemplate } from '../validations/emailTemplates.js';
+import { activationEmailTemplate, forgotPasswordEmailTemplate } from '../validations/emailTemplates.js';
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
-async function sendEmail(to, subject, url, text) {
+async function sendEmail(to, subject, url, firstName, kind) {
+  const html =
+    kind === 'account-activation'
+      ? activationEmailTemplate(url, firstName)
+      : kind === 'forgot-password'
+      ? forgotPasswordEmailTemplate(url, firstName)
+      : '';
+
   try {
     const data = await resend.emails.send({
       from: `Feli Express <${process.env.RESEND_NO_REPLY_EMIL}>`,
       // from: process.env.RESEND_NO_REPLY_EMIL,
       to: [to],
       subject,
-      html: activationEmailTemplate(url, text),
+      html,
     });
 
     if (data.error) throw new Error(data.error.message);
