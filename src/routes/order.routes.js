c2909d5cfa18ffbfff4cc27d964b25fdf5eb5f23
Eric Tuyizere Ndungutse <dav.ndungutse@gmail.com>
2024-08-16 09:55:20 +0200

    feat: Implement full text search (#255)

--- src/routes/order.routes.js
+++ src/routes/order.routes.js
@@ -1,12 +1,13 @@
 import express from 'express';
 import { isLoggedIn } from '../middlewares/authentication.js';
-import { getOrder, getOrders, updateOrder } from '../controllers/order.js';
+import { getOrder, getOrders, searchOrder, updateOrder } from '../controllers/order.js';
 import { restrictTo } from '../middlewares/authorization.js';
 
 const router = express.Router({ mergeParams: true });
 
 router.get('/', isLoggedIn, getOrders);
 
+router.get('/search', isLoggedIn, searchOrder);
 router.get('/:id', isLoggedIn, getOrder);
 router.patch('/:id', isLoggedIn, restrictTo('admin'), updateOrder);
 
